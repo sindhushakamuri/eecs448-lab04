f26dfbd67a60f8bdc4b9968bed812bdf8a42866b
Sindhu Shakamuri <sindhu.shakamuri@gmail.com>
2019-10-09 00:27:15 -0500

    initital commit for ex 1

---
+++ Exercise1/multiplication.php
@@ -0,0 +1,21 @@
+<?php
+echo "<table >";
+for ($row = 0; $row <= 100; $row++) {
+    echo "<tr> \n";
+    for ($col = 0; $col <= 100; $col++) {
+        if($row == 0 && $col == 0)
+            echo '<td>  </td>'; //inserts nothing
+        else if ($row == 0 && $col != 0)
+            echo "<td>$col</td>";
+        else if ($row != 0 && $col == 0)
+            echo "<td>$row</td>";
+        else {
+            $mul = $col * $row; 
+            echo "<td>$mul</td> \n";
+        }
+
+    }
+    echo "</tr>";
+}
+echo "</table>";
+?>
